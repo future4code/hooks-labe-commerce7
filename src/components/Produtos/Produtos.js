c19663d2a0dc8885145fdbce66c9212800880fa0
Bryan Fernandes <bryanfernandes8@gmail.com>
2022-05-12 22:59:39 -0300

    Refatorando e mudando aparência

--- src/components/Produtos/Produtos.js
+++ src/components/Produtos/Produtos.js
@@ -6,18 +6,21 @@ const Div = styled.div`
 display: flex;
 width: 670px;
 flex-wrap: wrap;
-/* background-color: lightgray; */
+background-color: #3e77b6;
+
 
 span{
     display: block;
 }
 @media(max-width: 400px) {
-    width: 400px;
+    width: 100vw;
+    flex-direction: column;
+    align-items: center;
   }
 `
 
 const DivProduto = styled.div`
-border: none;
+border: 1px solid #7c6ea7;
 border-radius: 5px;
 
 width: 200px;
@@ -26,9 +29,23 @@ display:flex;
 flex-direction: column;
 margin: 10px;
 height: 340px;
+text-align: center;
+background-color: #bbd2ec;
+
+
+color: #7c6ea7;
+
 
 box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.2);
+/* box-shadow: 0 0 2px 2px rgba(124, 110, 167, 0.2); */
+
 z-index: 1;
+
+
+img{
+    width: 200px;
+    height: 250px;
+}
 
 @media(max-width: 400px) {
     flex-direction: row;
@@ -38,59 +55,88 @@ z-index: 1;
 `
 
 const Button = styled.button`
-border: 1px solid black;
+border: 1px solid #7c6ea7;
 background-color: transparent;
 border-radius: 5px;
 margin-top: 10px;
 cursor: pointer;
+color: #7c6ea7;
 
 &:hover{
-    background-color: lightblue;
+    border: none;
+    background-color: #7c6ea7;
+    color: #bbd2ec;
+    -webkit-transition: background-color 500ms ease-in-out;
+    -ms-transition: background-color 500ms ease-in-out;
+    transition: background-color 500ms ease-in-out;
 }
 `
 
 const DivOrdenacao = styled.div`
 display: flex;
-justify-content: space-between;
+justify-content: space-evenly;
 width: 100%;
+background-color: #3e77b6;
+color: #bbd2ec;
+
+select{
+    border: 1px solid black;
+    background-color: transparent;
+    border-radius: 5px;
+
+}
+
+@media(max-width: 400px) {
+     flex-wrap: wrap;
+     div,span{
+         margin: 5px ;
+     }
+     
+  }
 `
 
 const Centralizar = styled.div`
 display: flex;
 justify-content: center;
+background-color: #3e77b6;
+
 `
 
 class Produtos extends React.Component{
     state = {
-        ordenacao: 'crescente'
+        ordenacao: 'crescente',
+        tipoOrdenacao: 'nome'
     }
 
     // método que verifica o tipo de ordenação
     ordenacao = (a, b)=> {
-            const nameA = a.nome.toUpperCase();
-            const nameB = b.nome.toUpperCase();
-                
-            // ordenacao crescente
-            if(this.state.ordenacao=== 'crescente'){if (nameA < nameB) {
-                return -1;
-                }
-                if (nameA > nameB) {
-                return 1;
-                }
+
+        // verificando tipo de ordenação
+        switch(this.state.tipoOrdenacao){
+           
+            // ordenando por preço
+            case 'preco':
+                let retornoParaCasoPreco = a.preco - b.preco
+                this.state.ordenacao==='crescente' || (retornoParaCasoPreco = retornoParaCasoPreco * -1)
+                return retornoParaCasoPreco
             
-                return 0;}
+            // por "default" oredena por nome
+            default:
+                // coloca o nome dos produtos em minúsculo
+                const nameA = a.nome.toUpperCase();
+                const nameB = b.nome.toUpperCase();
+                        
+                // retorno atribuido com valor "0" e contunua "0" caso os nomes comparados sejam iguais
+                let retorno = 0;
+        
+                // verifica qual nome vem antes ou depois
+                nameA === nameB || nameA > nameB? retorno = 1: retorno = -1
+        
+                // verifica tipo de ordenação
+                this.state.ordenacao==='crescente' || (retorno = retorno * -1)   
                 
-                // ordenacao "decrescete"
-                else{
-                    if (nameB < nameA) {
-                        return -1;
-                        }
-                        if (nameB > nameA) {
-                        return 1;
-                        }
-                    
-                        return 0; 
-                }
+                return retorno
+        }
       }
 
 
@@ -101,12 +147,13 @@ class Produtos extends React.Component{
             return produtoCarrinho.id === produto.id
         })
 
-        let carrinho = []
+        let carrinhoAuxiliar = []
 
-        // caso o produto já esteja no carrinho, aumenta sua qtd
+
+        // caso o produto já esteja no carrinho, aumenta sua 'qtd'
         if(contagem.length === 1){
 
-            carrinho = this.props.carrinho.map((produtoCarrinho)=>{
+            carrinhoAuxiliar = this.props.carrinho.map((produtoCarrinho)=>{
                 if(produtoCarrinho.id === produto.id){
                     return {...produtoCarrinho, qtd: contagem[0].qtd + 1}
                 }
@@ -114,21 +161,29 @@ class Produtos extends React.Component{
         }
         // caso não esteja adiciona ao carrinho com 'qtd=1'
         else{
-            carrinho = [...this.props.carrinho, {...produto, qtd: 1}]
+            carrinhoAuxiliar = [...this.props.carrinho, {...produto, qtd: 1}]
         }
 
         // chama o método da classe App para 'setar' o estado do carrinho no component App
-        this.props.addCarrinho(carrinho)
+        this.props.addCarrinho(carrinhoAuxiliar)
       }
 
     //método para "pegar" o evento de quando o usuário mudar o select "ordenação"
-    onChangeSelect = (event)=>{
+    onChangeSelectOrdenacao = (event)=>{
         this.setState({ordenacao: event.target.value})
+    }
+
+    // método para "pegar" o evento de quando o usuário mudar o select "ordenar por"
+    onChangeSelectTipoOrdenacao = (event)=>{
+        this.setState({tipoOrdenacao: event.target.value})
     }
 
 
     render(){
+        // lista de produtos filtrados ordenados de acordo com a função "this.orgenação"
         const ordenacaoProdutos = this.props.produtosFiltrados.sort(this.ordenacao)
+       
+        // Mapeamento dos produtos ordenados para exibir na tela 
         const produtosNaTela = ordenacaoProdutos.map((produto)=>{
             return <DivProduto key={produto.id}>
                 <div><img src={produto.img} alt='imagem teste' /></div> 
@@ -143,16 +198,32 @@ class Produtos extends React.Component{
         })
         return <Centralizar>
         <div>
-            <DivOrdenacao><span>quantidade de produtos: {this.props.produtosFiltrados.length} </span>
+            <DivOrdenacao>
+                <span>quantidade de produtos: {this.props.produtosFiltrados.length} </span>
                 <div>
-                <label>ordenação: </label>
-                <select value={this.state.ordenacao}
-                onChange={this.onChangeSelect}>
-                    <option value='crescente'>Crescente</option>
-                    <option value='decrescente'>Decrescente</option>
-                </select>
-                    </div>
-                </DivOrdenacao>
+                    {/* aqui escolhe tipo de ordenação*/}
+                    <label>ordenar por: </label>
+                    <select value={this.state.tipoOrdenacao}
+                    onChange={this.onChangeSelectTipoOrdenacao}>
+                        <option value='nome'>nome</option>
+                        <option value='preco'>Preço</option>
+                    </select>
+                </div>
+
+                <div>
+                    {/* aqui escolhe ordem */}
+                    <label>ordenação: </label>
+                    <select value={this.state.ordenacao}
+                    onChange={this.onChangeSelectOrdenacao}>
+                        <option value='crescente'>Crescente</option>
+                        <option value='decrescente'>Decrescente</option>
+                    </select>
+                </div>
+
+                {/* teste */}
+                {/* {this.props.carrinho.length>0 && `nome: ${this.props.carrinho[0].nome}` }
+                {this.props.carrinho.length>0 && `qtd: ${this.props.carrinho[0].qtd}` } */}
+            </DivOrdenacao>
             <Div>   
                 {produtosNaTela}
             </Div>
